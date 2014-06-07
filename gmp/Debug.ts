06aab4f43be38be977cd190deac2a1a4b6d4d75c
Alex Holkner <alex.holkner@gmail.com>
2014-06-07 11:46:09 +1000

    Debug menu

--- gmp/Debug.ts
+++ gmp/Debug.ts
@@ -1,12 +1,46 @@
-﻿module gmp {
+﻿/// <reference path="_References.ts" />
+
+module gmp {
+
+    class CheatToggleMenuItem extends MenuItem {
+        flag: string
+
+        constructor(name: string) {
+            super(name, '', () => { this.toggle() })
+            this.flag = name
+            this.refreshName()
+        }
+
+        private toggle() {
+            Debug[this.flag] = !Debug[this.flag]
+            this.refreshName()
+        }
+
+        private refreshName() {
+            var value: boolean = Debug[this.flag]
+            this.name = this.flag + ': ' + (value ? 'YES' : 'NO')
+        }
+    }
+
+    class CheatMenu extends Menu {
+        constructor(world: World) {
+            super(world)
+            this.enableInfo = false
+            this.items.push(new CheatToggleMenuItem('disableCollision'))
+            this.items.push(new CheatToggleMenuItem('disableRequire'))
+        }
+
+    }
 
     export class Debug {
 
         static onKeyPressed(key: crackle.Key) {
+            if (key == crackle.Key.c && document.location.hash.indexOf('debug') != -1)
+                game.world.pushMenu(new CheatMenu(game.world))
         }
 
         static disableCollision: boolean
-        static disableRequire: boolean = true
+        static disableRequire: boolean
     }
 
 }
